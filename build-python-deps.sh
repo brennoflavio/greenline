bde13085afb9f8866d847c292d5bb479a61cd6c7
brennoflavio <contato@brennoflavio.com.br>
2026-06-15 18:43:40 -0300

    Release from forgejo

--- build-python-deps.sh
+++ build-python-deps.sh
@@ -5,7 +5,12 @@ cd "$INSTALL_DIR"
 VENV_PATH="$BUILD_DIR/venv"
 export UV_PROJECT_ENVIRONMENT="$VENV_PATH"
 
-/usr/local/uv sync
+UV_BIN="${UV_INSTALL_DIR:-/usr/local}/uv"
+if [ ! -x "$UV_BIN" ]; then
+    UV_BIN="$(command -v uv)"
+fi
+
+"$UV_BIN" sync
 
 PYTHON_DIR=$(ls "$VENV_PATH/lib/")
 cp -r "$VENV_PATH/lib/$PYTHON_DIR/site-packages/"* .
